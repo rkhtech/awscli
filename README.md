4cf3c684702c6fd3f5d04579b773ada1cc3e20c9
rkhtech <randy@hommel.name>
2016-12-21 11:13:16 -0800

    aws-cli/1.11.32 Python/2.7.12 Linux/4.4.0-53-generic botocore/1.4.89

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # AWSCLI Version:
 ```
-aws-cli/1.11.30 Python/2.7.12 Linux/4.4.0-53-generic botocore/1.4.87
+aws-cli/1.11.32 Python/2.7.12 Linux/4.4.0-53-generic botocore/1.4.89
 ```
-## I used the following AWS documentation to deploy the awscli toolset:
+### I used the following AWS documentation to deploy the awscli toolset:
 [Installing the AWS Command Line Interface](http://docs.aws.amazon.com/cli/latest/userguide/installing.html)
--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ docker build --no-cache --pull -t rkhtech/awscli:latest .
 
 version=$(docker run -it rkhtech/awscli aws --version)
 oldversion=$(cat VERSION)
-echo -e '# AWSCLI Version:'"\n"'```'"\n$version\n"'```'"\n## I used the following AWS documentation to deploy the awscli toolset:\n[Installing the AWS Command Line Interface](http://docs.aws.amazon.com/cli/latest/userguide/installing.html)" > README.md
+echo -e '# AWSCLI Version:'"\n"'```'"\n$version\n"'```'"\n### I used the following AWS documentation to deploy the awscli toolset:\n[Installing the AWS Command Line Interface](http://docs.aws.amazon.com/cli/latest/userguide/installing.html)" > README.md
 echo "$version" > VERSION
 
 if [ "$version" != "$oldversion" ]; then

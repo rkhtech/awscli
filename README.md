e5a8eb8515d906536ed7034b30a3bde22c3cb725
rkhtech <randy@hommel.name>
2017-01-29 21:41:36 -0800

    aws-cli/1.11.44 Python/2.7.12 Linux/4.4.0-59-generic botocore/1.5.7

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ aws-cli/1.11.44 Python/2.7.12 Linux/4.4.0-59-generic botocore/1.5.7
 ```
 ### I used the following AWS documentation to install the awscli into the container:
 [Installing the AWS Command Line Interface](http://docs.aws.amazon.com/cli/latest/userguide/installing.html)
+### Docker Hub Location
+[rkhtech/awscli](https://hub.docker.com/r/rkhtech/awscli/)
--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ docker build --no-cache --pull -t rkhtech/awscli:latest .
 
 version=$(docker run -it rkhtech/awscli aws --version)
 oldversion=$(cat VERSION)
-echo -e '# AWSCLI Version:'"\n"'```'"\n$version\n"'```'"\n### I used the following AWS documentation to install the awscli into the container:\n[Installing the AWS Command Line Interface](http://docs.aws.amazon.com/cli/latest/userguide/installing.html)" > README.md
+echo -e '# AWSCLI Version:'"\n"'```'"\n$version\n"'```'"\n### I used the following AWS documentation to install the awscli into the container:\n[Installing the AWS Command Line Interface](http://docs.aws.amazon.com/cli/latest/userguide/installing.html)\n### Docker Hub Location\n[rkhtech/awscli](https://hub.docker.com/r/rkhtech/awscli/)" > README.md
 echo "$version" > VERSION
 
 if [ "$version" != "$oldversion" ]; then

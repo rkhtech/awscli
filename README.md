1646b7339161a2cf5be63561994e5014feba35e1
rkhtech <randy@hommel.name>
2017-01-31 10:01:52 -0800

    aws-cli/1.11.44 Python/2.7.13 Linux/4.4.0-59-generic botocore/1.5.7

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # AWSCLI Version:
-```
-aws-cli/1.11.44 Python/2.7.13 Linux/4.4.0-59-generic botocore/1.5.7
-```
+
 ### I used the following AWS documentation to install the awscli into the container:
 [Installing the AWS Command Line Interface](http://docs.aws.amazon.com/cli/latest/userguide/installing.html)
-### Docker Hub Location
-[rkhtech/awscli](https://hub.docker.com/r/rkhtech/awscli/)
+
+### Docker Hub Location\n[rkhtech/awscli](https://hub.docker.com/r/rkhtech/awscli/)
--- build.sh
+++ build.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
-password=$(cat /home/randy/.docker/loginpass.txt)
-
-### You should replace this line with the directory where the script is installed  (This is for cron simplicity)
-cd /home/randy/Dropbox/git/awscli
-
 docker build --no-cache --pull -t rkhtech/awscli:latest .
 
 version=$(docker run -it rkhtech/awscli aws --version)
 oldversion=$(cat VERSION)
-echo -e '# AWSCLI Version:'"\n"'```'"\n$version\n"'```'"\n### I used the following AWS documentation to install the awscli into the container:\n[Installing the AWS Command Line Interface](http://docs.aws.amazon.com/cli/latest/userguide/installing.html)\n### Docker Hub Location\n[rkhtech/awscli](https://hub.docker.com/r/rkhtech/awscli/)" > README.md
+#echo -e '# AWSCLI Version:'"\n"'```'"\n$version\n"'```'"\n### I used the following AWS documentation to install the awscli into the container:\n[Installing the AWS Command Line Interface](http://docs.aws.amazon.com/cli/latest/userguide/installing.html)\n### Docker Hub Location\n[rkhtech/awscli](https://hub.docker.com/r/rkhtech/awscli/)" > README.md
+
+cat > README.md << EOF
+# AWSCLI Version:
+```
+$version
+```
+### I used the following AWS documentation to install the awscli into the container:
+[Installing the AWS Command Line Interface](http://docs.aws.amazon.com/cli/latest/userguide/installing.html)
+
+### Docker Hub Location\n[rkhtech/awscli](https://hub.docker.com/r/rkhtech/awscli/)
+EOF
+
 echo "$version" > VERSION
 
 if [ "$version" != "$oldversion" ]; then
 	git add *
 	git commit -m "$version"
 	git push
-#	docker login -u rkhtech -p $password
-#	docker push rkhtech/awscli:latest
 else
 	echo "Version still the same.  Not updating repo."
 fi

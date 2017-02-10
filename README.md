227f8150f4a324dd8125f0b0a34b0ece8efd244c
rkhtech <randy@hommel.name>
2017-02-10 12:40:29 -0800

    aws-cli/1.11.46 Python/2.7.13 Linux/4.4.0-59-generic botocore/1.5.9

--- README.md
+++ README.md
@@ -27,4 +27,21 @@ $ alias aws='docker run -i rkhtech/awscli aws'
 $ aws --version
 aws-cli/1.11.46 Python/2.7.13 Linux/4.4.0-59-generic botocore/1.5.9
 ```
+*Note:* If this container is installed on an EC2 instance, credentials are granted by way of a Role assigned to the Instance it runs on.
 
+### Example using environment variables to pass credentials into container
+```
+docker run -i --rm 	-e AWS_DEFAULT_REGION=us-west-2 	-e AWS_ACCESS_KEY_ID=AKIAI67VIZ62T5I76Z6Q 	-e AWS_SECRET_ACCESS_KEY=DJl2b2B7HdcfU309WUF7zwc2ZRdHykqf2Jd3An2M 	rkhtech/awscli 	aws ec2 describe-instances
+```
+*Note:* These credentials have been removed and are no longer valid credentials.
+
+### Passing default region to container
+#### Passing as an environment variable to the container:
+```
+$ docker run -i --rm -e AWS_DEFAULT_REGION=us-west-2 rkhtech/awscli aws ec2 describe-instances
+```
+*OR*
+#### Passing as an option to the aws cli:
+```
+$ docker run -i --rm rkhtech/awscli aws ec2 describe-instances --region=us-west-2
+```
--- build.sh
+++ build.sh
@@ -35,7 +35,29 @@ $ alias aws='docker run -i rkhtech/awscli aws'
 $ aws --version
 $version
 \`\`\`
+*Note:* If this container is installed on an EC2 instance, credentials are granted by way of a Role assigned to the Instance it runs on.
 
+### Example using environment variables to pass credentials into container
+\`\`\`
+docker run -i --rm \
+	-e AWS_DEFAULT_REGION=us-west-2 \
+	-e AWS_ACCESS_KEY_ID=AKIAI67VIZ62T5I76Z6Q \
+	-e AWS_SECRET_ACCESS_KEY=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 \
+	rkhtech/awscli \
+	aws ec2 describe-instances
+\`\`\`
+*Note:* These credentials have been removed and are no longer valid credentials.
+
+### Passing default region to container
+#### Passing as an environment variable to the container:
+\`\`\`
+$ docker run -i --rm -e AWS_DEFAULT_REGION=us-west-2 rkhtech/awscli aws ec2 describe-instances
+\`\`\`
+*OR*
+#### Passing as an option to the aws cli:
+\`\`\`
+$ docker run -i --rm rkhtech/awscli aws ec2 describe-instances --region=us-west-2
+\`\`\`
 EOF
 
 echo "$version" > VERSION
